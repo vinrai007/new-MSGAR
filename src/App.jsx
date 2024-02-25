baa7cc976f354db6cb03091181da06f9b5835778
vinrai007 <129852101+vinrai007@users.noreply.github.com>
2024-02-25 16:06:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { UserContextProvider } from "./UserContext";
+import { MessagesProvider } from './MessageContext';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom"; // Import HashRouter instead of Router
+
+import ContactPage from "./ContactPage";
+import MessagePage from "./MessagePage";
+import Rotes from "./Routes";  // Assuming this is the correct import
+import './App.css';
+
+
+function App() {
+  axios.defaults.baseURL = 'http://localhost:4040';
+  // axios.defaults.baseURL = 'https://vchat-backend-cs72.onrender.com';
+  axios.defaults.withCredentials = true;
+
+  return (
+    <UserContextProvider>
+      <MessagesProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Rotes />} />
+          <Route path="/contacts" element={<ContactPage />} />
+          <Route path="/messages/:selectedUserId" element={<MessagePage />} />
+        </Routes>
+        </Router>
+      </MessagesProvider>
+    </UserContextProvider>
+  );
+}
+
+export default App;
